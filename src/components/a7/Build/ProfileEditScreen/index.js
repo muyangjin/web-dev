a485d45db6d63cd3a0cc799834fdb74975703b9c
Muyang Jin <muyang.jin@sap.com>
2021-11-09 22:18:51 -0800

    Minor update

--- src/components/a7/Build/ProfileEditScreen/index.js
+++ src/components/a7/Build/ProfileEditScreen/index.js
@@ -7,7 +7,7 @@ const ProfileEditScreen = () => {
     return(
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-2 col-xl-2">
-                <NavigationSidebar active="home"/>
+                <NavigationSidebar active="profile"/>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                  style={{"position": "relative"}}>
